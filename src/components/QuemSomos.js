30c3eecf3ccaf936d8cf881f0d2d50a5a0a27bd7
feScholucha <112656640+feScholucha@users.noreply.github.com>
2024-04-29 20:51:45 -0300

    Quem Somos: Versão Inicial
    Adicionado página quem somos com texto e imagem placeholder. Também foi adicionado classes no index que foram utilizadas

--- src/components/QuemSomos.js
+++ src/components/QuemSomos.js
@@ -1,14 +1,19 @@
 import { useState } from 'react'
+// import imgQmSomos from "./src/images/fotoQuemSomos.jpg"
+var imgQmSomos = require("../../src/images/fotoQuemSomos.jpg");
 
-const QuemSomos = ({onVoltarClick}) => {
+const QuemSomos = ({ onVoltarClick }) => {
     return (
-        <div className='quadrado'>
+        <div className='retangulo'>
+            <img src={imgQmSomos} alt="A Equipe" class="imagem-Qm-Somos" />
             <h1>Quem somos</h1>
+            <div className="retangulo">
+                <p>O Projeto Pontinha tem o objetivo de promover integração entre a comunidade universitária e crianças de escolas públicas da cidade de São Carlos, por meio de oficinas, proporcionando a elas aprendizado de maneira lúdica e dinâmica. É responsável por conectar jovens universitários, que buscam realizar trabalho voluntário, às crianças carentes, e mais que isso, ao participarem do projeto, os universitários criam competências como autoconfiança, empatia, liderança, entre muitos outros benefícios, sendo um sistema que favorece a todos. </p>
+            </div>
             <div className='botao-canto'>
                 <button onClick={onVoltarClick}>Voltar</button>
             </div>
         </div>
     );
 }
- 
 export default QuemSomos;
